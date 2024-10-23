e00e8f5bc8ddfa5e80e9155bfa4b20112628a179
Huzaim <huzaimasghar@gmail.com>
2024-10-23 14:48:07 +0500

    2nd part completed - UI Design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+export const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+    </div>
+  )
+}
+
+//STARTER
+// import React from 'react'
+// import Conversation from './Conversation'
+
+// export const Conversations = () => {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//     </div>
+//   )
+// }
